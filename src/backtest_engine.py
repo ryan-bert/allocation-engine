6243db2317c2e97073a30e8b19a55b604dafa980
ryan-bert <ryanberti66@gmail.com>
2025-01-15 21:41:07 +0200

    Designed class structure

---
+++ src/backtest_engine.py
@@ -0,0 +1,10 @@
+class Engine:
+
+    def __init__(self, weights, returns, initial_capital):
+        pass
+
+    def run(self, start_date, end_date, fee_rate=0):
+        pass
+
+    def get_results(self):
+        pass
