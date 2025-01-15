3b3f3ef1135c61b4aee495f69f139f9b2167624c
ryan-bert <ryanberti66@gmail.com>
2025-01-15 21:48:42 +0200

    Created __init__()

--- src/backtest_engine.py
+++ src/backtest_engine.py
@@ -1,7 +1,13 @@
 class Engine:
 
-    def __init__(self, weights, returns, initial_capital):
-        pass
+    def __init__(self, weights, returns, initial_capital=1):
+
+        self.weights_df = weights
+        self.returns_df = returns
+        self.initial_capital = initial_capital
+
+        self.portfolio_df = None
+        self.results = None
 
     def prepare(self):
         pass
