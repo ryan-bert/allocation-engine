e1e7390acdd2b9101830398e1296f6654ebf6f8b
ryan-bert <ryanberti66@gmail.com>
2025-01-15 23:05:53 +0200

    Added to prepare() method

--- src/backtest_engine.py
+++ src/backtest_engine.py
@@ -9,10 +9,17 @@ class Engine:
         self.portfolio_df = None
         self.results = None
 
-    def prepare(self):
-        pass
+    def prepare(self, start_date, end_date):
 
-    def run(self, start_date, end_date, fee_rate=0):
+        # Filter the data to the specified date range
+        self.weights_df = self.weights_df[(self.weights_df.index >= start_date) & (self.weights_df.index <= end_date)]
+        self.returns_df = self.returns_df[(self.returns_df.index >= start_date) & (self.returns_df.index <= end_date)]
+
+        #! HANDLE MISSING DATA - DATES MUST BE ALIGNED
+            # - Maybe drop rows with missing data
+            # - Maybe forward fill weights
+
+    def run(self, fee_rate=0):
         pass
 
     def get_results(self):
