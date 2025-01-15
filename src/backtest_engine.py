5cf5a25f02a1eb712eda0ae615a4adb2f16b1880
ryan-bert <ryanberti66@gmail.com>
2025-01-15 21:42:51 +0200

    Added prepare() empty method

--- src/backtest_engine.py
+++ src/backtest_engine.py
@@ -3,6 +3,9 @@ class Engine:
     def __init__(self, weights, returns, initial_capital):
         pass
 
+    def prepare(self):
+        pass
+
     def run(self, start_date, end_date, fee_rate=0):
         pass
 
