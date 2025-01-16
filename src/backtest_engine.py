e4ebc551557e30fc0da7585b16fa1d89c856bbaf
ryan-bert <ryanberti66@gmail.com>
2025-01-16 08:30:15 +0200

    Alligned dates

--- src/backtest_engine.py
+++ src/backtest_engine.py
@@ -9,15 +9,25 @@ class Engine:
         self.portfolio_df = None
         self.results = None
 
-    def prepare(self, start_date, end_date):
+    def prepare(self, start_date, end_date, mon_to_fri_df):
 
         # Filter the data to the specified date range
         self.weights_df = self.weights_df[(self.weights_df.index >= start_date) & (self.weights_df.index <= end_date)]
+        self.weights_df.rweset_index(drop=True, inplace=True)
         self.returns_df = self.returns_df[(self.returns_df.index >= start_date) & (self.returns_df.index <= end_date)]
+        self.returns_df.reset_index(drop=True, inplace=True)
 
-        #! HANDLE MISSING DATA - DATES MUST BE ALIGNED
-            # - Maybe drop rows with missing data
-            # - Maybe forward fill weights
+        # Filter mon_to_fri_df to the specified date range
+        mon_to_fri_df = mon_to_fri_df[(mon_to_fri_df.index >= start_date) & (mon_to_fri_df.index <= end_date)]
+
+        # Align the dates of weights by forward filling
+        self.weights_df = self.weights_df.merge(mon_to_fri_df, left_index=True, right_index=True, how="right")
+        self.weights_df.fillna(method="ffill", inplace=True)
+
+        # Align the dates of returns by filling with zeros
+        self.returns_df = self.returns_df.merge(mon_to_fri_df, left_index=True, right_index=True, how="right")
+        self.returns_df.fillna(0, inplace=True)
+
 
     def run(self, fee_rate=0):
         pass
